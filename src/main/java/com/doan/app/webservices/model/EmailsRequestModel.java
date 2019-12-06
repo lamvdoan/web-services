1852dea73cce74bab04153a8fe80f2dc4e230d7a
lamvdoan <lamvdoan@gmail.com>
2019-12-05 23:00:44 -0600

    added setter to the request model

--- src/main/java/com/doan/app/webservices/model/EmailsRequestModel.java
+++ src/main/java/com/doan/app/webservices/model/EmailsRequestModel.java
@@ -1,11 +1,13 @@
 package com.doan.app.webservices.model;
 
-import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 
-@XmlRootElement
 public class EmailsRequestModel {
     private List<String> emails;
+
+    public void setEmails(List<String> emails) {
+        this.emails = emails;
+    }
 
     public List<String> getEmails() {
         return emails;
